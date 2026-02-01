3b23590d6959ec13019fd254b11f4e7b6a61daff
Dirk Thierbach <thierbach@synerva.de>
2026-02-01 07:34:12 +0100

    Fix ROM size.

--- tools/rom.py
+++ tools/rom.py
@@ -3,27 +3,30 @@
 # combine rom files
 
 # carom:
-# ../../../tools/rom.py 0 11a9a71f.bin dcb77b22.bin 
+# ../../../tools/rom.py 0 0x800 11a9a71f.bin dcb77b22.bin 
 
 # romca
-# ../../../tools/rom.py 0xc00 706e035f.bin 47f8b402.bin 799546a7.bin 33f1a400.bin 
+# ../../../tools/rom.py 0xc00 0x800 706e035f.bin 47f8b402.bin 799546a7.bin 33f1a400.bin 
 
 import sys
 
 outname="out.bin"
 
 ofs = int(sys.argv[1], 0)
+size = int(sys.argv[2], 0)
 with open(outname, mode="wb") as outfile:
-    for highname, lowname in zip(sys.argv[2::2], sys.argv[3::2]):
+    for highname, lowname in zip(sys.argv[3::2], sys.argv[4::2]):
         print(f"Combining high={highname} low={lowname}")
         with open(highname, mode="rb") as highfile:
             with open(lowname, mode="rb") as lowfile:
                 highfile.seek(ofs)
                 lowfile.seek(ofs)
-                while True:
+                pos = 0
+                while pos < size:
                     highbuf = highfile.read(1)
                     lowbuf = lowfile.read(1)
                     if len(highbuf) < 1 or len(lowbuf) < 1:
                         break
                     outfile.write(highbuf)
                     outfile.write(lowbuf)
+                    pos = pos + 1
